b68c66faca2de84a9d5bee8a0a97f7e61335d4ac
kostiy <konstchered@mail.com>
2021-07-22 13:47:32 +0300

    first commit

---
+++ lesson 1/js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://sonerl.com/${category}/5`);
+
+const user = "David";
+
+alert(`Привет, ${user}`);
+
+let incr = 10,
+    dect = 10;
+
+//-- incr++;
+//-- dect--;
+
+console.log(--incr);
+console.log(++dect);
+
+console.log(5 % 2);
+
+console.log(2 * 4 == 8);
+
+const   isChecked = true;
+isClose = true;
+        
+console.log();
